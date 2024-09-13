dc2c5e9d0df582840b8fa43ffd350b4b46ae5df5
romandicesarefernandes <154945549+romandicesarefernandes@users.noreply.github.com>
2024-09-13 00:58:39 -0400

    develop pushed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,106 +1,34 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap, linkdin, githubblack  } from '../assets';
+import { shaq } from '../assets';
 
 const Hero = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
-        <img
-          src={bwmap}
-          alt="world map"
-          className="w-full h-full sm:block hidden object-cover overflow-hidden"
-        />
-      </div>
-      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen overflow-hidden">
-        <img
-          src={worldmap}
-          alt="world map"
-          className="w-full h-full sm:hidden block object-cover overflow-hidden"
-        />
-      </div>
-      <section
-        className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
-        sm:bg-hero bg-hero-mobile overflow-hidden">
-        <div
-          className={`absolute inset-0 sm:top-[250px] top-[150px] 
-          lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
-          max-w-7xl mx-auto flex flex-row items-start
-          justify-between gap-3 overflow-hidden`}>
-          <div className="flex flex-col justify-center items-center mt-5 ml-3 overflow-hidden">
-            <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden overflow-hidden" />
-            <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden overflow-hidden" />
-          </div>
+      <section className="relative flex flex-col w-full h-screen mx-auto bg-black mt-40 justify-center items-center">
 
-          <div>
-            <h1
-              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
-              <span
-                className="sm:text-battleGray sm:text-[90px] 
-                text-eerieBlack text-[50px] font-mova
-                font-extrabold uppercase">
-                Roman
-              </span>
-            </h1>
-            <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Computer Science undergraduate. <br className="sm:block hidden" />
-              At the University Of Central Florida.
-            </p>
-            <div className="flex">
-  <a href="www.linkedin.com/in/roman-di-cesare-994641309">
-    <img src={linkdin} className="w-20 h-20" />
-  </a>
-  <a href="https://github.com/romandicesarefernandes">
-    <img src={githubblack} className="w-20 h-20" />
-  </a>
+        <div className = "flex bg-black justify-center items-center ">  
+        <p className="text-6xl bg-gradient-to-l from-black to-gray-300 bg-clip-text text-transparent">
+  Hello world
+</p><div className="relative inline-block">
+  <img
+    src={shaq}
+    alt="pineapple"
+    className="h-96 w-96 filter brightness-80"
+  />
+  <div className="absolute inset-0 bg-black opacity-40 pointer-events-none"></div>
 </div>
-            
-            
-           
-          </div>
-          <div
-            className="w-screen flex flex-col items-start 
-            justify-center sm:-ml-[3rem] xxs:mt-4"></div>
 
-          <div></div>
-        </div>
 
-        <div
-          className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
-          <a href="#about">
-            <div
-              className="w-[35px] h-[64px] rounded-3xl border-4 
-            border-french border-dim flex
-            justify-center items-start p-2">
-              <motion.div
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                }}
-                className="w-3 h-3 rounded-full bg-taupe mb-1"
-              />
-            </div>
-          </a>
+<p className="text-6xl bg-gradient-to-r from-black to-gray-300 bg-clip-text text-transparent">
+  Hello world
+</p>
         </div>
+        
+     
 
-        {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
-        <div>
-        <img
-  className="absolute bottom-7 ml-[30vw] 
-  lg:ml-[55vw] md:ml-[40vw] xmd:ml-[40vw] 2xl:ml-[63vw]
-  sm:h-[0vh] md:h-[40vh] xl:h-[40vh] hidden md:block"
-  src={shaq}
-  alt="shaquille"
-/>
-        </div>
+
       </section>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-2 fixed 
-      top-0 z-20 bg-flashWhite sm:opacity-[0.97] xxs:h-[12vh] `}>
+    className={`${styles.paddingX} w-full flex items-center py-2 fixed 
+    top-0 z-20 bg-black  sm:opacity-[0.97] xxs:h-[12vh]`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
